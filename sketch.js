a66ea34d7f2ba6e0f3e5bd3ab3b212d118b57828
inescrisostomo <96204642+inescrisostomo@users.noreply.github.com>
2022-01-26 16:24:41 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+a=0; 
+diametro=100
+
+
+function setup() {
+  
+  createCanvas(windowWidth, windowHeight);
+  colorMode(RGB);
+  background(0);
+  
+
+}
+
+function draw() {
+  
+  background("rgba(0,0,0,0.02)");
+ 
+  a++;
+
+  fill("rgba(0, 0, 0, 1)");
+  stroke("rgba(250, 250, 250, 0.5)");
+  ellipse(a+(diametro/2), windowHeight/2 +(sin(a/50)*150), diametro, diametro);
+  
+
+
+  }
