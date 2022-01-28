4a3e8fb553ca3ec52788e793b214769e99fea1d2
inescrisostomo <96204642+inescrisostomo@users.noreply.github.com>
2022-01-28 23:10:23 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
 a=0; 
 diametro=100
 
-function setup() {
+function setup() {  
   
   createCanvas(windowWidth, windowHeight);
+  
   colorMode(RGB);
   background(0);
+  
   fill("rgba(0, 0, 0, 0.01)");
   stroke("rgba(250, 250, 250, 0.2)");  
+  
 }
-function draw() {
-   for (let i=0; i<10; i++){
+
+function draw() {  
+  background("rgba(0,0,0,0.2)");
+   for (let i=0; i<60; i++){
    ellipse((a+(diametro/2))*2-50, windowHeight/2 +(sin(a/50)*200)*2, diametro, diametro);
    a++;
+     
   }
 }
